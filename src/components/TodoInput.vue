2c88833c3817ef75d10bce2ec89fcd8024a81265
oscarsaravia <oscarsaraviard0013@gmail.com>
2024-02-28 20:11:38 -0600

    chore: started implementing todo input

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+  const newTodo = ref('');
+</script>
+
+<template>
+  <form action="">
+    <input
+      type="text"
+      placeholder="Add a new task..."
+      v-model="newTodo"
+    />
+    <button>Add</button>
+  </form>
+  <h1>{{ newTodo }}</h1>
+</template>
+
+<style scoped>
+  input {
+    border-radius: 10px;
+    padding: 10px;
+    width: 70%;
+    font-size: 1.5rem;
+  }
+  form {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+  }
+  button {
+    border-radius: 10px;
+    padding: 10px;
+    width:100px;
+    font-size: 1.5rem;
+    margin-left: 10px;
+    background-color: #BC6C25;
+  }
+  input, button {
+    height: 50px;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TodoInput from '@/components/TodoInput.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="main-container">
+    <TodoInput />
+  </div>
 </template>
+
+<style scoped>
+  h1 {
+    color: #283618;
+  }
+  p {
+    color: #606C38;
+  }
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+  }
+</style>
